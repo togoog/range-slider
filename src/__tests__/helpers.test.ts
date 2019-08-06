71a7a7ac9f5989d5e342cae97c44c9ad22953ce0
Roman Korenev <romengrus@gmail.com>
2019-08-06 16:34:29 +0200

    include stop in range

--- src/__tests__/helpers.test.ts
+++ src/__tests__/helpers.test.ts
@@ -6,8 +6,8 @@ describe('range', () => {
     expect([...range(0, 10, 0)]).toEqual([]);
   });
 
-  test('Should return empty sequence if start == step', () => {
-    expect([...range(10, 10)]).toEqual([]);
+  test('Should return [start] (sequence with 1 number) if start == stop', () => {
+    expect([...range(10, 10)]).toEqual([10]);
   });
 
   test('Should return empty sequence if start < step & step < 0', () => {
@@ -18,26 +18,36 @@ describe('range', () => {
     expect([...range(10, 0, 1)]).toEqual([]);
   });
 
-  test('Should return growing arithmetic progression when: start < stop && 0 < step < (stop - start) ', () => {
+ test('Should return growing sequence when: start < stop && 0 < step < (stop - start) ', () => {
     fc.assert(
       fc.property(fc.integer(-1000, 1000), fc.integer(0, 100), fc.integer(1, 100), (start, total, step) => {
         const stop = start + total;
         const rangeArr = [...range(start, stop, step)];
-        expect(rangeArr.length).toBe(Math.ceil(total / step));
-        for (let i = 1; i < rangeArr.length; i += 1) {
+
+        // should include stop
+        expect(rangeArr.length).toBe(Math.ceil(total / step) + 1);
+        rangeArr[rangeArr.length - 1] === stop;
+
+        // should be growing
+        for (let i = 1; i < rangeArr.length - 1; i += 1) {
           expect(rangeArr[i] - rangeArr[i - 1]).toBe(step);
         }
       }),
     );
   });
 
-  test('Should return declining arithmetic progression when: start > stop && (stop - start) < step < 0', () => {
+  test('Should return declining sequence when: start > stop && (stop - start) < step < 0', () => {
     fc.assert(
       fc.property(fc.integer(-1000, 1000), fc.integer(0, 100), fc.integer(-100, -1), (start, total, step) => {
         const stop = start - total;
         const rangeArr = [...range(start, stop, step)];
-        expect(rangeArr.length).toBe(Math.ceil(total / -step));
-        for (let i = 1; i < rangeArr.length; i += 1) {
+
+        // should include stop
+        expect(rangeArr.length).toBe(Math.ceil(total / -step) + 1);
+        rangeArr[rangeArr.length - 1] === stop;
+
+        // should be declining
+        for (let i = 1; i < rangeArr.length - 1; i += 1) {
           expect(rangeArr[i] - rangeArr[i - 1]).toBe(step);
         }
       }),
--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@
  * Generate arithmetic progression generator.
  *
  * @param start range start (included)
- * @param stop range stop (not included)
+ * @param stop range stop (included)
  * @param step delta between numbers
  */
 export function* range(start: number, stop?: number, step: number = 1): IterableIterator<number> {
@@ -13,12 +13,17 @@ export function* range(start: number, stop?: number, step: number = 1): Iterable
   }
 
   // return if sequence can not be generated with provided parameters
-  if (step === 0 || start === stop || (step > 0 && start > stop) || (step < 0 && start < stop)) return;
+  if (step === 0 || (step > 0 && start > stop) || (step < 0 && start < stop)) return;
 
   let current = start;
 
-  while (step > 0 ? current < stop : current > stop) {
+  while (step > 0 ? current <= stop : current >= stop) {
     yield current;
     current += step;
+  }
+
+  // add stop as a final number in sequence
+  if (current - step !== stop) {
+    yield stop;
   }
 }
