d15957bc5f0745b6fd102c3c8e6c695927abd572
Roman Korenev <romengrus@gmail.com>
2019-09-24 00:37:08 +0200

    remove prettier. Will format code manually

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,19 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   env: {
     browser: true,
-    jest: true,
+    jest: true
   },
   extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:@typescript-eslint/recommended" // Uses the recommended rules from the @typescript-eslint/eslint-plugin
   ],
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: "module"
   },
   rules: {
-    semi: ['error', 'always'],
-    complexity: ['error', 5],
-    '@typescript-eslint/explicit-function-return-type': 0,
-  },
+    semi: ["error", "always"],
+    complexity: ["error", 5],
+    "@typescript-eslint/explicit-function-return-type": 0
+  }
 };
