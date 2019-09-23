da723b0d92e4bcc7a9ed83c971a161061bfc278f
Roman Korenev <romengrus@gmail.com>
2019-09-23 02:08:33 +0200

    add some helper functions

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,30 +1,64 @@
+import { option } from 'fp-ts';
+import { pipe } from 'fp-ts/lib/pipeable';
+
 /**
- * Generate sequence of numbers
- * TODO: can work with float numbers
- *
- * @param start range start (included)
- * @param stop range stop (included)
- * @param step delta between numbers
+ * Create object from key value pairs
+ * @param entries array of key value pairs
  */
-export function* genSequence(start: number, stop?: number, step: number = 1): IterableIterator<number> {
-  // when only 1 parameter passed
-  if (typeof stop === 'undefined') {
-    stop = start;
-    start = 0;
-  }
-
-  // return if sequence can not be generated with provided parameters
-  if (step === 0 || (step > 0 && start > stop) || (step < 0 && start < stop)) return;
-
-  let current = start;
-
-  while (step > 0 ? current <= stop : current >= stop) {
-    yield current;
-    current += step;
-  }
-
-  // add stop as a final number in sequence
-  if (current - step !== stop) {
-    yield stop;
-  }
+function objectFromEntries<T>(entries: [string, T][]) {
+  return Object.assign(
+    {},
+    ...Array.from(entries, ([k, v]: [string, T]) => ({ [k]: v })),
+  );
 }
+
+//
+// ─── DOM MANIPULATION ───────────────────────────────────────────────────────────
+//
+
+/**
+ * Query dom elements
+ * @param selector css selector
+ */
+function $(selector: string): option.Option<Element[]> {
+  return pipe(
+    option.tryCatch(() => document.querySelectorAll(selector)),
+    option.chain(v =>
+      v.length === 0 ? option.none : option.some(Array.from(v)),
+    ),
+  );
+}
+
+/**
+ * Get attributes from dom element
+ * @param el dom element
+ */
+function getAttributes(el: Element): option.Option<RangeSliderAttributes> {
+  const entries: [keyof RangeSliderOptions, string | null][] = [];
+
+  // global options
+  entries.push(['value', el.getAttribute('value')]);
+  entries.push(['min', el.getAttribute('min')]);
+  entries.push(['max', el.getAttribute('max')]);
+  entries.push(['step', el.getAttribute('step')]);
+  entries.push(['padding', el.getAttribute('data-padding')]);
+  entries.push(['orientation', el.getAttribute('data-orientation')]);
+  entries.push(['direction', el.getAttribute('data-direction')]);
+  entries.push(['isDisabled', el.getAttribute('data-is-disabled')]);
+  entries.push(['isPolyfill', el.getAttribute('data-is-polyfill')]);
+  entries.push(['locale', el.getAttribute('data-locale')]);
+  entries.push(['cssPrefix', el.getAttribute('data-css-prefix')]);
+  entries.push(['cssClasses', el.getAttribute('data-css-classes')]);
+
+  // components
+  entries.push(['grid', el.getAttribute('data-grid')]);
+  entries.push(['handles', el.getAttribute('data-handles')]);
+  entries.push(['intervals', el.getAttribute('data-intervals')]);
+  entries.push(['tooltips', el.getAttribute('data-tooltips')]);
+
+  const result = objectFromEntries(entries.filter(entry => entry[1] !== null));
+
+  return option.some(result);
+}
+
+export { $, getAttributes, objectFromEntries };
