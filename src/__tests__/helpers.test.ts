cb4f748d92f7379c2169a486061ccd5814e342d0
Roman Korenev <romengrus@gmail.com>
2019-09-23 02:08:33 +0200

    add some helper functions

--- src/__tests__/helpers.test.ts
+++ src/__tests__/helpers.test.ts
@@ -1,55 +1,102 @@
-import * as fc from 'fast-check';
-import { genSequence } from '../helpers';
+import { isSome, isNone, fold, some } from 'fp-ts/lib/Option';
+import { $, getAttributes } from '../helpers';
 
-describe('range', () => {
-  test('Should return empty sequence if step == 0', () => {
-    expect([...genSequence(0, 10, 0)]).toEqual([]);
+describe('$', () => {
+  test('Should find existing elements', () => {
+    document.body.innerHTML = `
+      <input type="range" class="range-slider" />
+      <input type="range" class="range-slider" />
+      <input type="range" class="range-slider" />
+    `;
+    const selector = '.range-slider';
+    const elements = $(selector);
+    expect(isSome(elements)).toBe(true);
+    expect(fold(() => 0, (v: Element[]) => v.length)(elements)).toBe(3);
   });
 
-  test('Should return [start] (sequence with 1 number) if start == stop', () => {
-    expect([...genSequence(10, 10)]).toEqual([10]);
+  test('Should return none when no elements found', () => {
+    document.body.innerHTML = `
+      <span>some random text ...</span>
+    `;
+    const selector = '.range-slider';
+    const elements = $(selector);
+    expect(isNone(elements)).toBe(true);
   });
 
-  test('Should return empty sequence if start < step & step < 0', () => {
-    expect([...genSequence(0, 10, -1)]).toEqual([]);
+  test('Should return none if selector is not valid', () => {
+    document.body.innerHTML = `
+      <input type="range" class="range-slider" />
+      <input type="range" class="range-slider" />
+      <input type="range" class="range-slider" />
+    `;
+    const selector = `..range-slider`;
+    const getElements = () => $(selector);
+    expect(getElements).not.toThrow();
+    expect(isNone(getElements())).toBe(true);
   });
 
-  test('Should return empty sequence if start > step & step > 0', () => {
-    expect([...genSequence(10, 0, 1)]).toEqual([]);
+  test('Should return none if selector is empty string', () => {
+    document.body.innerHTML = `
+      <input type="range" class="range-slider" />
+      <input type="range" class="range-slider" />
+      <input type="range" class="range-slider" />
+    `;
+    const selector = '';
+    const getElements = () => $(selector);
+    expect(getElements).not.toThrow();
+    expect(isNone(getElements())).toBe(true);
+  });
+});
+
+describe('getAttributes', () => {
+  test('Should return empty object when no attributes set on element', () => {
+    document.body.innerHTML = '<input type="range" id="range-slider" />';
+    const el = document.getElementById('range-slider');
+    const attributes = el && getAttributes(el);
+    expect(attributes).toEqual(some({}));
   });
 
- test('Should return growing sequence when: start < stop && 0 < step < (stop - start) ', () => {
-    fc.assert(
-      fc.property(fc.integer(-1000, 1000), fc.integer(0, 100), fc.integer(1, 100), (start, total, step) => {
-        const stop = start + total;
-        const rangeArr = [...genSequence(start, stop, step)];
-
-        // should include stop
-        expect(rangeArr.length).toBe(Math.ceil(total / step) + 1);
-        rangeArr[rangeArr.length - 1] === stop;
-
-        // should be growing
-        for (let i = 1; i < rangeArr.length - 1; i += 1) {
-          expect(rangeArr[i] - rangeArr[i - 1]).toBe(step);
-        }
-      }),
-    );
-  });
-
-  test('Should return declining sequence when: start > stop && (stop - start) < step < 0', () => {
-    fc.assert(
-      fc.property(fc.integer(-1000, 1000), fc.integer(0, 100), fc.integer(-100, -1), (start, total, step) => {
-        const stop = start - total;
-        const rangeArr = [...genSequence(start, stop, step)];
-
-        // should include stop
-        expect(rangeArr.length).toBe(Math.ceil(total / -step) + 1);
-        rangeArr[rangeArr.length - 1] === stop;
-
-        // should be declining
-        for (let i = 1; i < rangeArr.length - 1; i += 1) {
-          expect(rangeArr[i] - rangeArr[i - 1]).toBe(step);
-        }
+  test('Should return Option<RangeSliderAttributes> if attributes are set', () => {
+    document.body.innerHTML = `
+      <input type="range"
+             id="range-slider"
+             value="50"
+             min="10"
+             max="100"
+             step="5"
+             data-orientation="vertical"
+             data-direction="rtl"
+             data-locale="ru-RU"
+             data-padding="3"
+             data-is-disabled="false"
+             data-is-polyfill="false"
+             data-css-prefix="rs"
+             data-css-classes="{'container': 'ctr', 'isHorizontal': 'hor', 'isVertical': 'ver'}"
+             data-handles="null"
+             data-tooltips="false"
+             data-intervals="false"
+             data-grid="true" />
+    `;
+    const el = document.getElementById('range-slider');
+    const attributes = el && getAttributes(el);
+    expect(attributes).toEqual(
+      some({
+        value: '50',
+        min: '10',
+        max: '100',
+        step: '5',
+        orientation: 'vertical',
+        direction: 'rtl',
+        locale: 'ru-RU',
+        padding: '3',
+        isDisabled: 'false',
+        isPolyfill: 'false',
+        cssPrefix: 'rs',
+        cssClasses: `{'container': 'ctr', 'isHorizontal': 'hor', 'isVertical': 'ver'}`,
+        handles: 'null',
+        tooltips: 'false',
+        intervals: 'false',
+        grid: 'true',
       }),
     );
   });
