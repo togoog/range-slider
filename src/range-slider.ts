ae7ddb00562b9b9569802ad2c6117c6cae4e7cb5
Roman Korenev <romengrus@gmail.com>
2019-09-29 02:18:35 +0200

    add typesafe getter & settter to RangeSlider

--- src/range-slider.ts
+++ src/range-slider.ts
@@ -1,66 +1,26 @@
-import { option } from 'fp-ts';
-import { pipe } from 'fp-ts/lib/pipeable';
-import { $ } from './helpers';
-
 const defaultOptions: RangeSliderOptions = {
   value: 50,
   min: 0,
   max: 100,
   step: 1,
   orientation: 'horizontal',
-  locale: 'en-US',
-  direction: 'ltr',
-  isDisabled: false,
-  isPolyfill: false,
-  padding: 0,
-  cssPrefix: 'range-slider-',
-  cssClasses: {
-    container: 'container',
-    isHorizontal: 'horizontal',
-    isVertical: 'vertical',
-    isDraggable: 'draggable',
-    isDragged: 'state-drag',
-    isActive: 'active',
-    isDisabled: 'disabled',
-  },
-  handles: [null],
-  tooltips: [true],
-  intervals: [true, false],
-  grid: {
-    isVisible: false,
-    formatter: () => '',
-    generator: () => '',
-  },
+  tooltips: true,
 };
 
-class RangeSlider implements RangeSlider {
+class RangeSlider implements RangeSliderPlugin {
   constructor(
-    el: string | Element | Element[] | NodeList,
-    options: Partial<RangeSliderOptions> = defaultOptions,
-  ) {
-    // get dom elements that should be transformed to range sliders
-    const elements =
-      typeof el === 'string'
-        ? $(el)
-        : el instanceof Element
-        ? option.some([el])
-        : el instanceof NodeList
-        ? option.some(Array.from(el) as Element[])
-        : el.every(v => v instanceof Element)
-        ? option.some(el)
-        : option.none;
+    private el: string,
+    private options: RangeSliderOptions = defaultOptions,
+  ) {}
 
-    pipe(
-      elements,
-      option.map(this.init),
-    );
+  get<T extends OptionsKey>(key: T): RangeSliderOptions[T] {
+    return this.options[key];
   }
 
-  /**
-   * Init RangeSlider for each provided element
-   * @param elements dome elements
-   */
-  private init(elements: Element[]) {}
+  set<T extends OptionsKey>(key: T, value: RangeSliderOptions[T]): RangeSlider {
+    this.options[key] = value;
+    return this;
+  }
 }
 
-export default RangeSlider;
+export { RangeSlider };
