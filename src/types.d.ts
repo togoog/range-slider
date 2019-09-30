a6cb6ed1c947e3f3bcdad2ac5fa29c2664145c48
Roman Korenev <romengrus@gmail.com>
2019-09-30 04:09:28 +0200

    add ModelData type

--- src/types.d.ts
+++ src/types.d.ts
@@ -9,13 +9,26 @@ type RangeSliderOptions = {
 
 type OptionsKey = keyof RangeSliderOptions;
 
-interface RangeSliderPlugin {
+type RangeSliderPlugin = {
   get<T extends OptionsKey>(key: T): RangeSliderOptions[T];
   set<T extends OptionsKey>(
     key: T,
     value: RangeSliderOptions[T],
   ): RangeSliderPlugin;
-}
+};
+
+type RangeSliderModel = {
+  propose(data: Partial<ModelData>): void;
+};
+
+type ModelData = {
+  value: [number] | [number, number];
+  min: number;
+  max: number;
+  step: number;
+  orientation: 'horizontal' | 'vertical';
+  tooltips: [boolean] | [boolean, boolean];
+};
 
 type Events = {
   init: void;
