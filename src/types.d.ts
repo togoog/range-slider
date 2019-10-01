b86a5c25c3af288c4131d72a30699d2c6cccee8e
Roman Korenev <romengrus@gmail.com>
2019-10-01 03:25:28 +0200

    refactored types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,10 +1,10 @@
 type RangeSliderOptions = {
-  value: number | [number, number];
+  value: number[];
   min: number;
   max: number;
   step: number;
   orientation: 'horizontal' | 'vertical';
-  tooltips: boolean | [boolean, boolean];
+  tooltips: boolean[];
 };
 
 type OptionsKey = keyof RangeSliderOptions;
@@ -18,43 +18,11 @@ type RangeSliderPlugin = {
 };
 
 type RangeSliderModel = {
-  propose(data: Partial<ModelData>): void;
+  propose(data: Partial<ModelProposal>): void;
 };
 
-type ModelData = {
-  value: [number] | [number, number];
-  min: number;
-  max: number;
-  step: number;
-  orientation: 'horizontal' | 'vertical';
-  tooltips: [boolean] | [boolean, boolean];
-};
+type ModelData = RangeSliderOptions;
 
-type Events = {
-  init: void;
-  destroy: void;
-
-  /**
-   * Use this event when synchronizing the slider value to another element, such as an  <input>.
-   * It fires every time the slider values are changed, either by a user or by calling API methods.
-   * In most cases, this event should be more convenient than the 'slide' event.
-   */
-  update: void;
-
-  /**
-   * This event fires when a handle is clicked (mousedown, or the equivalent touch events).
-   */
-  slide_start: void;
-
-  /**
-   * This event is useful when you specifically want to listen to a handle being dragged,
-   * but want to ignore other updates to the slider value. This event also fires on a change by a 'tap'.
-   * In most cases, the 'update' is the better choice.
-   */
-  slide: void;
-
-  /**
-   * This event is the opposite of the 'start' event. If fires when a handle is released (mouseup etc),
-   */
-  slide_end: void;
+type ModelProposal = {
+  [key in keyof ModelData]: (data: ModelData) => ModelData[key];
 };
