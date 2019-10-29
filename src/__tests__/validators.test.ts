fc680e7ffe78f7895c0ea2b848562f51cfaecee8
Roman Korenev <romengrus@gmail.com>
2019-10-29 18:41:06 +0200

    refactor validators

--- src/__tests__/validators.test.ts
+++ src/__tests__/validators.test.ts
@@ -1,4 +1,3 @@
-import { Options } from '../types';
 import * as fc from 'fast-check';
 import {
   //errors
@@ -8,7 +7,8 @@ import {
   errNotValidStep,
   errNotValidOrientation,
   errNotValidTooltips,
-  errIncorrectShape,
+  errNotValidIntervals,
+  errOptionsIsNotAnObject,
   // validators
   checkValue,
   checkMin,
@@ -16,6 +16,7 @@ import {
   checkStep,
   checkOrientation,
   checkTooltips,
+  checkIntervals,
   checkRangeSliderOptions,
 } from '../validators';
 import { Just, Nothing } from 'purify-ts/Maybe';
@@ -37,31 +38,16 @@ describe('checkValue', () => {
   });
 
   test('non numeric value is not valid', () => {
-    expect(checkValue('foo')).toEqual(Just(errNotValidValue('foo')));
-    expect(checkValue(true)).toEqual(Just(errNotValidValue(true)));
-    expect(checkValue(false)).toEqual(Just(errNotValidValue(false)));
-    expect(checkValue(null)).toEqual(Just(errNotValidValue(null)));
-    expect(checkValue(undefined)).toEqual(Just(errNotValidValue(undefined)));
-    expect(checkValue(NaN)).toEqual(Just(errNotValidValue(NaN)));
-    expect(checkValue({ foo: 123 })).toEqual(
-      Just(errNotValidValue({ foo: 123 })),
-    );
-  });
-
-  test('non numeric pair is not valid', () => {
-    expect(checkValue(['foo', 'bar'])).toEqual(
-      Just(errNotValidValue(['foo', 'bar'])),
-    );
-    expect(checkValue([null, undefined])).toEqual(
-      Just(errNotValidValue([null, undefined])),
-    );
-    expect(checkValue([true, false])).toEqual(
-      Just(errNotValidValue([true, false])),
-    );
-    expect(checkValue([123, 'bar'])).toEqual(
-      Just(errNotValidValue([123, 'bar'])),
-    );
-    expect(checkValue([NaN, 0])).toEqual(Just(errNotValidValue([NaN, 0])));
+    expect(checkValue('foo')).toEqual(Just(errNotValidValue()));
+    expect(checkValue(true)).toEqual(Just(errNotValidValue()));
+    expect(checkValue(false)).toEqual(Just(errNotValidValue()));
+    expect(checkValue(null)).toEqual(Just(errNotValidValue()));
+    expect(checkValue(undefined)).toEqual(Just(errNotValidValue()));
+    expect(checkValue(NaN)).toEqual(Just(errNotValidValue()));
+    expect(checkValue({ foo: 123 })).toEqual(Just(errNotValidValue()));
+    expect(checkValue(['a', 10])).toEqual(Just(errNotValidValue()));
+    expect(checkValue(['a', 'b'])).toEqual(Just(errNotValidValue()));
+    expect(checkValue([true, false])).toEqual(Just(errNotValidValue()));
   });
 });
 
@@ -76,13 +62,13 @@ describe('checkMin', () => {
   });
 
   test('non numeric values are not valid', () => {
-    expect(checkMin('foo')).toEqual(Just(errNotValidMin('foo')));
-    expect(checkMin(true)).toEqual(Just(errNotValidMin(true)));
-    expect(checkMin(false)).toEqual(Just(errNotValidMin(false)));
-    expect(checkMin(null)).toEqual(Just(errNotValidMin(null)));
-    expect(checkMin(undefined)).toEqual(Just(errNotValidMin(undefined)));
-    expect(checkMin(NaN)).toEqual(Just(errNotValidMin(NaN)));
-    expect(checkMin({ foo: 123 })).toEqual(Just(errNotValidMin({ foo: 123 })));
+    expect(checkMin('foo')).toEqual(Just(errNotValidMin()));
+    expect(checkMin(true)).toEqual(Just(errNotValidMin()));
+    expect(checkMin(false)).toEqual(Just(errNotValidMin()));
+    expect(checkMin(null)).toEqual(Just(errNotValidMin()));
+    expect(checkMin(undefined)).toEqual(Just(errNotValidMin()));
+    expect(checkMin(NaN)).toEqual(Just(errNotValidMin()));
+    expect(checkMin({ foo: 123 })).toEqual(Just(errNotValidMin()));
   });
 });
 
@@ -97,13 +83,13 @@ describe('checkMax', () => {
   });
 
   test('non numeric values are not valid', () => {
-    expect(checkMax('foo')).toEqual(Just(errNotValidMax('foo')));
-    expect(checkMax(true)).toEqual(Just(errNotValidMax(true)));
-    expect(checkMax(false)).toEqual(Just(errNotValidMax(false)));
-    expect(checkMax(null)).toEqual(Just(errNotValidMax(null)));
-    expect(checkMax(undefined)).toEqual(Just(errNotValidMax(undefined)));
-    expect(checkMax(NaN)).toEqual(Just(errNotValidMax(NaN)));
-    expect(checkMax({ foo: 123 })).toEqual(Just(errNotValidMax({ foo: 123 })));
+    expect(checkMax('foo')).toEqual(Just(errNotValidMax()));
+    expect(checkMax(true)).toEqual(Just(errNotValidMax()));
+    expect(checkMax(false)).toEqual(Just(errNotValidMax()));
+    expect(checkMax(null)).toEqual(Just(errNotValidMax()));
+    expect(checkMax(undefined)).toEqual(Just(errNotValidMax()));
+    expect(checkMax(NaN)).toEqual(Just(errNotValidMax()));
+    expect(checkMax({ foo: 123 })).toEqual(Just(errNotValidMax()));
   });
 });
 
@@ -118,15 +104,13 @@ describe('checkStep', () => {
   });
 
   test('non numeric values are not valid', () => {
-    expect(checkStep('foo')).toEqual(Just(errNotValidStep('foo')));
-    expect(checkStep(true)).toEqual(Just(errNotValidStep(true)));
-    expect(checkStep(false)).toEqual(Just(errNotValidStep(false)));
-    expect(checkStep(null)).toEqual(Just(errNotValidStep(null)));
-    expect(checkStep(undefined)).toEqual(Just(errNotValidStep(undefined)));
-    expect(checkStep(NaN)).toEqual(Just(errNotValidStep(NaN)));
-    expect(checkStep({ foo: 123 })).toEqual(
-      Just(errNotValidStep({ foo: 123 })),
-    );
+    expect(checkStep('foo')).toEqual(Just(errNotValidStep()));
+    expect(checkStep(true)).toEqual(Just(errNotValidStep()));
+    expect(checkStep(false)).toEqual(Just(errNotValidStep()));
+    expect(checkStep(null)).toEqual(Just(errNotValidStep()));
+    expect(checkStep(undefined)).toEqual(Just(errNotValidStep()));
+    expect(checkStep(NaN)).toEqual(Just(errNotValidStep()));
+    expect(checkStep({ foo: 123 })).toEqual(Just(errNotValidStep()));
   });
 });
 
@@ -137,17 +121,13 @@ describe('checkOrientation', () => {
     expect(checkOrientation('horizontal')).toEqual(Nothing);
     expect(checkOrientation('vertical')).toEqual(Nothing);
     expect(checkOrientation('  vertical  ')).toEqual(
-      Just(errNotValidOrientation('  vertical  ')),
+      Just(errNotValidOrientation()),
     );
-    expect(checkOrientation('foo')).toEqual(
-      Just(errNotValidOrientation('foo')),
-    );
-    expect(checkOrientation(123)).toEqual(Just(errNotValidOrientation(123)));
-    expect(checkOrientation(NaN)).toEqual(Just(errNotValidOrientation(NaN)));
-    expect(checkOrientation(null)).toEqual(Just(errNotValidOrientation(null)));
-    expect(checkOrientation(undefined)).toEqual(
-      Just(errNotValidOrientation(undefined)),
-    );
+    expect(checkOrientation('foo')).toEqual(Just(errNotValidOrientation()));
+    expect(checkOrientation(123)).toEqual(Just(errNotValidOrientation()));
+    expect(checkOrientation(NaN)).toEqual(Just(errNotValidOrientation()));
+    expect(checkOrientation(null)).toEqual(Just(errNotValidOrientation()));
+    expect(checkOrientation(undefined)).toEqual(Just(errNotValidOrientation()));
   });
 });
 
@@ -159,53 +139,64 @@ describe('checkTooltips', () => {
     expect(checkTooltips(false)).toEqual(Nothing);
   });
 
-  test('pair of booleans are valid', () => {
-    expect(checkTooltips([true, true])).toEqual(Nothing);
+  test('array of booleans are valid', () => {
+    expect(checkTooltips([false])).toEqual(Nothing);
+    expect(checkTooltips([true])).toEqual(Nothing);
     expect(checkTooltips([true, false])).toEqual(Nothing);
-    expect(checkTooltips([false, true])).toEqual(Nothing);
-    expect(checkTooltips([false, false])).toEqual(Nothing);
+    expect(checkTooltips([true, true, false])).toEqual(Nothing);
+    expect(checkTooltips([true, false, true, false])).toEqual(Nothing);
   });
 
   test('non boolean values are not valid', () => {
-    expect(checkTooltips('foo')).toEqual(Just(errNotValidTooltips('foo')));
-    expect(checkTooltips(null)).toEqual(Just(errNotValidTooltips(null)));
-    expect(checkTooltips(undefined)).toEqual(
-      Just(errNotValidTooltips(undefined)),
-    );
-    expect(checkTooltips(123)).toEqual(Just(errNotValidTooltips(123)));
-    expect(checkTooltips({ foo: true })).toEqual(
-      Just(errNotValidTooltips({ foo: true })),
-    );
+    expect(checkTooltips('foo')).toEqual(Just(errNotValidTooltips()));
+    expect(checkTooltips(null)).toEqual(Just(errNotValidTooltips()));
+    expect(checkTooltips(undefined)).toEqual(Just(errNotValidTooltips()));
+    expect(checkTooltips(123)).toEqual(Just(errNotValidTooltips()));
+    expect(checkTooltips({ foo: true })).toEqual(Just(errNotValidTooltips()));
+    expect(checkTooltips([true, 1])).toEqual(Just(errNotValidTooltips()));
+    expect(checkTooltips([true, 'a'])).toEqual(Just(errNotValidTooltips()));
+  });
+});
+
+// ────────────────────────────────────────────────────────────────────────────────
+
+describe('checkIntervals', () => {
+  test('booleans are valid', () => {
+    expect(checkIntervals(true)).toEqual(Nothing);
+    expect(checkIntervals(false)).toEqual(Nothing);
   });
 
-  test('any pair of non boolean values is not valid', () => {
-    expect(checkTooltips(['abc', true])).toEqual(
-      Just(errNotValidTooltips(['abc', true])),
-    );
-    expect(checkTooltips([true, 123])).toEqual(
-      Just(errNotValidTooltips([true, 123])),
-    );
-    expect(checkTooltips(['foo', null])).toEqual(
-      Just(errNotValidTooltips(['foo', null])),
-    );
-    expect(checkTooltips([undefined, []])).toEqual(
-      Just(errNotValidTooltips([undefined, []])),
-    );
+  test('array of booleans are valid', () => {
+    expect(checkIntervals([false])).toEqual(Nothing);
+    expect(checkIntervals([true])).toEqual(Nothing);
+    expect(checkIntervals([true, false])).toEqual(Nothing);
+    expect(checkIntervals([true, true, false])).toEqual(Nothing);
+    expect(checkIntervals([true, false, true, false])).toEqual(Nothing);
+  });
+
+  test('non boolean values are not valid', () => {
+    expect(checkIntervals('foo')).toEqual(Just(errNotValidIntervals()));
+    expect(checkIntervals(null)).toEqual(Just(errNotValidIntervals()));
+    expect(checkIntervals(undefined)).toEqual(Just(errNotValidIntervals()));
+    expect(checkIntervals(123)).toEqual(Just(errNotValidIntervals()));
+    expect(checkIntervals({ foo: true })).toEqual(Just(errNotValidIntervals()));
+    expect(checkIntervals([true, 1])).toEqual(Just(errNotValidIntervals()));
+    expect(checkIntervals([true, 'a'])).toEqual(Just(errNotValidIntervals()));
   });
 });
 
 // ────────────────────────────────────────────────────────────────────────────────
 
 describe('checkRangeSliderOptions', () => {
-  test('should return Left(Error) for primitive values', () => {
+  test('non object values are not valid', () => {
     expect(checkRangeSliderOptions('foo')).toEqual(
-      Left([errIncorrectShape('foo')]),
+      Left([errOptionsIsNotAnObject()]),
     );
     expect(checkRangeSliderOptions(null)).toEqual(
-      Left([errIncorrectShape(null)]),
+      Left([errOptionsIsNotAnObject()]),
     );
     expect(checkRangeSliderOptions(undefined)).toEqual(
-      Left([errIncorrectShape(undefined)]),
+      Left([errOptionsIsNotAnObject()]),
     );
   });
 
@@ -222,120 +213,6 @@ describe('checkRangeSliderOptions', () => {
       v => {
         expect(checkRangeSliderOptions(v)).toEqual(Right(v));
       },
-    );
-  });
-
-  test('should return Left([Error]) if value is not valid', () => {
-    const options: { [key in keyof Options]: unknown } = {
-      value: 'foo',
-      min: 0,
-      max: 100,
-      step: 5,
-      orientation: 'horizontal',
-      tooltips: true,
-    };
-
-    expect(checkRangeSliderOptions(options)).toEqual(
-      Left([errNotValidValue('foo')]),
-    );
-  });
-
-  test('should return Left([Error, Error]) if value, min are not valid', () => {
-    const options: { [key in keyof Options]: unknown } = {
-      value: 'foo',
-      min: 'bar',
-      max: 100,
-      step: 5,
-      orientation: 'horizontal',
-      tooltips: true,
-    };
-
-    expect(checkRangeSliderOptions(options)).toEqual(
-      Left([errNotValidValue('foo'), errNotValidMin('bar')]),
-    );
-  });
-
-  test('should return Left([Error, Error, Error]) if value, min, max are not valid', () => {
-    const options: { [key in keyof Options]: unknown } = {
-      value: 'foo',
-      min: 'bar',
-      max: 'zoo',
-      step: 5,
-      orientation: 'horizontal',
-      tooltips: true,
-    };
-
-    expect(checkRangeSliderOptions(options)).toEqual(
-      Left([
-        errNotValidValue('foo'),
-        errNotValidMin('bar'),
-        errNotValidMax('zoo'),
-      ]),
-    );
-  });
-
-  test('should return Left([Error, Error, Error, Error]) if value, min, max, step are not valid', () => {
-    const options: { [key in keyof Options]: unknown } = {
-      value: 'foo',
-      min: 'bar',
-      max: 'zoo',
-      step: 'no-way',
-      orientation: 'horizontal',
-      tooltips: true,
-    };
-
-    expect(checkRangeSliderOptions(options)).toEqual(
-      Left([
-        errNotValidValue('foo'),
-        errNotValidMin('bar'),
-        errNotValidMax('zoo'),
-        errNotValidStep('no-way'),
-      ]),
-    );
-  });
-
-  test(`should return Left([Error, Error, Error, Error, Error]) 
-    if value, min, max, step, orientation are not valid`, () => {
-    const options: { [key in keyof Options]: unknown } = {
-      value: 'foo',
-      min: 'bar',
-      max: 'zoo',
-      step: 'no-way',
-      orientation: 'lesbian',
-      tooltips: true,
-    };
-
-    expect(checkRangeSliderOptions(options)).toEqual(
-      Left([
-        errNotValidValue('foo'),
-        errNotValidMin('bar'),
-        errNotValidMax('zoo'),
-        errNotValidStep('no-way'),
-        errNotValidOrientation('lesbian'),
-      ]),
-    );
-  });
-
-  test(`should return Left([Error, Error, Error, Error, Error, Error]) 
-    if value, min, max, step, orientation, tooltips are not valid`, () => {
-    const options: { [key in keyof Options]: unknown } = {
-      value: 'foo',
-      min: 'bar',
-      max: 'zoo',
-      step: 'no-way',
-      orientation: 'lesbian',
-      tooltips: null,
-    };
-
-    expect(checkRangeSliderOptions(options)).toEqual(
-      Left([
-        errNotValidValue('foo'),
-        errNotValidMin('bar'),
-        errNotValidMax('zoo'),
-        errNotValidStep('no-way'),
-        errNotValidOrientation('lesbian'),
-        errNotValidTooltips(null),
-      ]),
     );
   });
 });
--- src/validators.ts
+++ src/validators.ts
@@ -1,43 +1,8 @@
-import { Options } from './types';
-import { all, allPass, anyPass, partial } from 'ramda';
-import { isNotObject, isPair } from 'ramda-adjunct';
+import { Options, RangeSliderError } from './types';
+import { all, allPass, anyPass } from 'ramda';
+import { isNotObject, isArray } from 'ramda-adjunct';
 import { Maybe, Just, Nothing } from 'purify-ts/Maybe';
 import { Either, Left, Right } from 'purify-ts/Either';
-import {
-  errNotANumber,
-  errNotANumberOrPairOfNumbers,
-  errNotOneOf,
-  errNotABooleanOrPairOfBooleans,
-  errIncorrectObjectShape,
-} from './errors';
-
-//
-// ─── ERRORS ─────────────────────────────────────────────────────────────────────
-//
-
-const errNotValidValue = partial(errNotANumberOrPairOfNumbers, [
-  'RangeSliderOptions["value"]',
-]);
-
-const errNotValidMin = partial(errNotANumber, ['RangeSliderOptions["min"]']);
-
-const errNotValidMax = partial(errNotANumber, ['RangeSliderOptions["max"]']);
-
-const errNotValidStep = partial(errNotANumber, ['RangeSliderOptions["step"]']);
-
-const errNotValidOrientation = partial(errNotOneOf, [
-  'RangeSliderOptions["orientation"]',
-  ['horizontal', 'vertical'],
-]);
-
-const errNotValidTooltips = partial(errNotABooleanOrPairOfBooleans, [
-  'RangeSliderOptions["tooltips"]',
-]);
-
-const errIncorrectShape = partial(errIncorrectObjectShape, [
-  'RangeSliderOptions',
-  ['value', 'min', 'max', 'step', 'orientation', 'tooltips'],
-]);
 
 //
 // ─── HELPERS ────────────────────────────────────────────────────────────────────
@@ -51,63 +16,143 @@ function isBoolean(v: unknown): v is boolean {
   return typeof v === 'boolean';
 }
 
-function isPairOfNumbers(v: unknown): v is [number, number] {
-  return allPass([isPair, all(isNumber)])(v);
+function isArrayOfNumbers(v: unknown): v is [number, number] {
+  return allPass([isArray, all(isNumber)])(v);
 }
 
-function isPairOfBooleans(v: unknown): v is [boolean, boolean] {
-  return allPass([isPair, all(isBoolean)])(v);
+function isArrayOfBooleans(v: unknown): v is [boolean, boolean] {
+  return allPass([isArray, all(isBoolean)])(v);
+}
+
+//
+// ─── ERRORS ─────────────────────────────────────────────────────────────────────
+//
+
+const errorPrefix = 'RangeSlider/Options';
+
+const ErrorNotValidValue = `${errorPrefix}/ErrorNotValidValue`;
+const ErrorNotValidMin = `${errorPrefix}/ErrorNotValidMin`;
+const ErrorNotValidMax = `${errorPrefix}/ErrorNotValidMax`;
+const ErrorNotValidStep = `${errorPrefix}/ErrorNotValidStep`;
+const ErrorNotValidOrientation = `${errorPrefix}/ErrorNotValidOrientation`;
+const ErrorNotValidTooltips = `${errorPrefix}/ErrorNotValidTooltips`;
+const ErrorNotValidIntervals = `${errorPrefix}/ErrorNotValidIntervals`;
+const ErrorOptionsIsNotAnObject = `${errorPrefix}/ErrorOptionsIsNotAnObject`;
+
+function errNotValidValue(): RangeSliderError {
+  return {
+    id: ErrorNotValidValue,
+    desc: `(value should be either number or array of numbers)`,
+  };
+}
+
+function errNotValidMin(): RangeSliderError {
+  return {
+    id: ErrorNotValidMin,
+    desc: `min should be a number)`,
+  };
+}
+
+function errNotValidMax(): RangeSliderError {
+  return {
+    id: ErrorNotValidMax,
+    desc: `(max should be a number)`,
+  };
+}
+
+function errNotValidStep(): RangeSliderError {
+  return {
+    id: ErrorNotValidStep,
+    desc: `(step should be a number)`,
+  };
+}
+
+function errNotValidOrientation(): RangeSliderError {
+  return {
+    id: ErrorNotValidOrientation,
+    desc: `(orientation should be one of: 'horizontal' or 'vertical')`,
+  };
+}
+
+function errNotValidTooltips(): RangeSliderError {
+  return {
+    id: ErrorNotValidTooltips,
+    desc: `(tooltips should be boolean or array of booleans)`,
+  };
+}
+
+function errNotValidIntervals(): RangeSliderError {
+  return {
+    id: ErrorNotValidIntervals,
+    desc: `(intervals should be boolean or array of booleans)`,
+  };
+}
+
+function errOptionsIsNotAnObject(): RangeSliderError {
+  return {
+    id: ErrorOptionsIsNotAnObject,
+    desc: `(options should be object)`,
+  };
 }
 
 //
 // ─── VALIDATORS ─────────────────────────────────────────────────────────────────
 //
 
-function checkValue(v: unknown): Maybe<Error> {
-  return anyPass([isNumber, isPairOfNumbers])(v)
+function checkValue(v: unknown): Maybe<RangeSliderError> {
+  return anyPass([isNumber, isArrayOfNumbers])(v)
     ? Nothing
-    : Just(errNotValidValue(v));
+    : Just(errNotValidValue());
 }
 
-function checkMin(v: unknown): Maybe<Error> {
-  return isNumber(v) ? Nothing : Just(errNotValidMin(v));
+function checkMin(v: unknown): Maybe<RangeSliderError> {
+  return isNumber(v) ? Nothing : Just(errNotValidMin());
 }
 
-function checkMax(v: unknown): Maybe<Error> {
-  return isNumber(v) ? Nothing : Just(errNotValidMax(v));
+function checkMax(v: unknown): Maybe<RangeSliderError> {
+  return isNumber(v) ? Nothing : Just(errNotValidMax());
 }
 
-function checkStep(v: unknown): Maybe<Error> {
-  return isNumber(v) ? Nothing : Just(errNotValidStep(v));
+function checkStep(v: unknown): Maybe<RangeSliderError> {
+  return isNumber(v) ? Nothing : Just(errNotValidStep());
 }
 
-function checkOrientation(v: unknown): Maybe<Error> {
+function checkOrientation(v: unknown): Maybe<RangeSliderError> {
   return v === 'horizontal' || v === 'vertical'
     ? Nothing
-    : Just(errNotValidOrientation(v));
+    : Just(errNotValidOrientation());
 }
 
-function checkTooltips(v: unknown): Maybe<Error> {
-  return isBoolean(v) || isPairOfBooleans(v)
+function checkTooltips(v: unknown): Maybe<RangeSliderError> {
+  return isBoolean(v) || isArrayOfBooleans(v)
     ? Nothing
-    : Just(errNotValidTooltips(v));
+    : Just(errNotValidTooltips());
 }
 
-function checkRangeSliderOptions(v: unknown): Either<Error[], Options> {
+function checkIntervals(v: unknown): Maybe<RangeSliderError> {
+  return isBoolean(v) || isArrayOfBooleans(v)
+    ? Nothing
+    : Just(errNotValidIntervals());
+}
+
+function checkRangeSliderOptions(
+  v: unknown,
+): Either<RangeSliderError[], Options> {
   if (isNotObject(v)) {
-    return Left([errIncorrectShape(v)]);
+    return Left([errOptionsIsNotAnObject()]);
   }
 
   // pretend that v is RangeSliderOptions (for typings to work)
   const options = v as Options;
 
-  const validationResults: Maybe<Error>[] = [];
+  const validationResults: Maybe<RangeSliderError>[] = [];
   validationResults.push(checkValue(options.value));
   validationResults.push(checkMin(options.min));
   validationResults.push(checkMax(options.max));
   validationResults.push(checkStep(options.step));
   validationResults.push(checkOrientation(options.orientation));
   validationResults.push(checkTooltips(options.tooltips));
+  validationResults.push(checkIntervals(options.intervals));
 
   const errors = Maybe.catMaybes(validationResults);
 
@@ -122,7 +167,8 @@ export {
   errNotValidStep,
   errNotValidOrientation,
   errNotValidTooltips,
-  errIncorrectShape,
+  errNotValidIntervals,
+  errOptionsIsNotAnObject,
   // validators
   checkValue,
   checkMin,
@@ -130,5 +176,6 @@ export {
   checkStep,
   checkOrientation,
   checkTooltips,
+  checkIntervals,
   checkRangeSliderOptions,
 };
