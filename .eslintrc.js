03e3a5a2b2072cd948867ae91bb4d030314de1a6
Roman Korenev <romengrus@gmail.com>
2019-11-16 17:32:00 +0200

    Add airbnb styleguide rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,20 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
   env: {
     browser: true,
     jest: true,
   },
   extends: [
+    'airbnb',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
