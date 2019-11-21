2076851f6fb8490b5d1c95792c50fdd3d333b348
Roman Korenev <romengrus@gmail.com>
2019-11-21 20:13:16 +0200

    Add Data type generator

--- src/__tests__/arbitraries.ts
+++ src/__tests__/arbitraries.ts
@@ -1,5 +1,8 @@
 import * as fc from 'fast-check';
-import { Options } from '../types';
+import { prop, indexBy, pluck } from 'ramda';
+import { isEven } from 'ramda-adjunct';
+import { Options, Data, HandleData, TooltipData, IntervalData } from '../types';
+import { createId, closestToStep } from '../helpers';
 
 function sortedArrayArb(minLength = 1, maxLength = 10) {
   return fc
@@ -26,9 +29,9 @@ function makeOptions() {
         max,
         step,
         cssClass: 'range-slider',
-        tooltips: value.map(v => v % 2 === 0),
+        tooltips: value.map(isEven),
         tooltipFormatter: (value: number) => value.toLocaleString(),
-        intervals: [...value, value[0]].map(v => v % 2 === 0),
+        intervals: [...value, value[0]].map(isEven),
         orientation,
         grid: {
           isVisible: value.reduce((acc, cur) => acc + cur, 0) > 0,
@@ -38,4 +41,76 @@ function makeOptions() {
     });
 }
 
-export { sortedArrayArb, makeOptions };
+function makeData() {
+  return fc
+    .record({
+      value: sortedArrayArb(),
+      orientation: fc.constantFrom('horizontal', 'vertical'),
+      numCells: fc.array(fc.nat(100), 1, 3),
+      seed: fc.nat(100),
+    })
+    .map(({ value, orientation, numCells, seed }) => {
+      const min = Math.round(value[0] - seed);
+      const max = Math.round(value[value.length - 1] + seed);
+      const step = Math.min(max - min, seed);
+
+      const handlesList = value.map(
+        (val, idx): HandleData => ({
+          id: createId('handle', idx),
+          value: closestToStep(min, max, step, val),
+          tooltipId: createId('tooltip', idx),
+          lhsIntervalId: createId('interval', idx),
+          rhsIntervalId: createId('interval', idx + 1),
+        }),
+      );
+
+      const tooltipsList = value.map(isEven).map(
+        (val, idx): TooltipData => ({
+          id: createId('tooltip', idx),
+          isVisible: val,
+          handleId: createId('handle', idx),
+        }),
+      );
+
+      const intervalsList = [...value, value[0]].map(isEven).map(
+        (val, idx): IntervalData => ({
+          id: createId('interval', idx),
+          isVisible: val,
+          lhsHandleId: idx > 0 ? createId('handle', idx - 1) : null,
+          rhsHandleId: idx < value.length ? createId('handle', idx) : null,
+        }),
+      );
+
+      return {
+        min,
+        max,
+        step,
+        orientation,
+        cssClass: 'range-slider',
+
+        // Handles
+        handleIds: pluck('id', handlesList),
+        handleDict: indexBy(prop('id'), handlesList),
+        activeHandleId: seed < 50 ? null : handlesList[0].id,
+
+        // Tooltips
+        tooltipIds: pluck('id', tooltipsList),
+        tooltipDict: indexBy(prop('id'), tooltipsList),
+        tooltipFormatter: (value: number) => value.toLocaleString(),
+        // TODO: find a way to simulate tooltipCollisions
+        tooltipCollisions: [],
+
+        // Intervals
+        intervalIds: pluck('id', intervalsList),
+        intervalDict: indexBy(prop('id'), intervalsList),
+
+        // Grid
+        grid: {
+          isVisible: value.reduce((acc, cur) => acc + cur, 0) > 0,
+          numCells,
+        },
+      } as Data;
+    });
+}
+
+export { sortedArrayArb, makeOptions, makeData };
