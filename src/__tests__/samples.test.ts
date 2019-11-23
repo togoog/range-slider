b20335c36db6a972301f9e346147849a9de25e24
Roman Korenev <romengrus@gmail.com>
2019-11-23 23:33:36 +0200

    Remove redundant test file

--- src/__tests__/samples.test.ts
+++
@@ -1,8 +0,0 @@
-import * as fc from 'fast-check';
-import { inspect } from 'util';
-import { makeData } from './arbitraries';
-
-test('samples', () => {
-  // const result = fc.sample(makeData(), 5);
-  // console.log(inspect(result, { depth: 5 }));
-});
