861e19514532463adaacabe26187d94300b960d2
Roman Korenev <romengrus@gmail.com>
2019-11-24 03:28:47 +0200

    Change interval untit tests to property based tests

--- src/__tests__/components/interval.test.ts
+++ src/__tests__/components/interval.test.ts
@@ -1,212 +1,40 @@
-import { State } from '../../types';
+import * as fc from 'fast-check';
+import { prop } from 'ramda';
+import { makeState } from '../arbitraries';
 import { View } from '../../mvp';
+import { convertOrientationToOrigin } from '../../converters';
 
-const cssClass = `range-slider`;
-const trackCSSClass = `${cssClass}__track`;
-const intervalCSSClass = `${cssClass}__interval`;
-const handleCSSClass = `${cssClass}__handle`;
-const tooltipCSSClass = `${cssClass}__tooltip`;
+const intervalCSSClass = 'range-slider__interval';
 
-const state: State = {
-  cssClass,
-  track: { orientation: 'horizontal', cssClass: trackCSSClass, role: 'track' },
-  intervals: [
-    {
-      id: 'interval_0',
-      from: 0,
-      to: 20,
-      cssClass: intervalCSSClass,
-      orientation: 'horizontal',
-      isVisible: false,
-      role: 'interval',
-    },
-    {
-      id: 'interval_1',
-      from: 20,
-      to: 40,
-      cssClass: intervalCSSClass,
-      orientation: 'horizontal',
-      isVisible: true,
-      role: 'interval',
-    },
-    {
-      id: 'interval_2',
-      from: 40,
-      to: 100,
-      cssClass: intervalCSSClass,
-      orientation: 'horizontal',
-      isVisible: false,
-      role: 'interval',
-    },
-  ],
-  handles: [
-    {
-      id: 'handle_0',
-      position: 20,
-      cssClass: handleCSSClass,
-      orientation: 'horizontal',
-      isActive: false,
-      role: 'handle',
-    },
-    {
-      id: 'handle_1',
-      position: 40,
-      cssClass: handleCSSClass,
-      orientation: 'horizontal',
-      isActive: false,
-      role: 'handle',
-    },
-  ],
-  tooltips: [
-    {
-      id: 'tooltip_0',
-      position: 20,
-      content: '20',
-      orientation: 'horizontal',
-      cssClass: tooltipCSSClass,
-      isVisible: true,
-      hasCollisions: false,
-      role: 'tooltip',
-    },
-    {
-      id: 'tooltip_1',
-      position: 40,
-      content: '40',
-      orientation: 'horizontal',
-      cssClass: tooltipCSSClass,
-      isVisible: true,
-      hasCollisions: false,
-      role: 'tooltip',
-    },
-  ],
-  grid: {
-    cssClass: 'range-slider__grid',
-    isVisible: true,
-    orientation: 'horizontal',
-    cells: [],
-    min: 0,
-    max: 100,
-    role: 'grid',
-  },
-};
-describe('Interval', () => {
-  test('interval should be hidden if isVisible = false', () => {
-    document.body.innerHTML = '<div id="root"></div>';
-    const el = document.querySelector('#root');
-    const view = new View(el as HTMLElement);
-    let intervals = document.getElementsByClassName(intervalCSSClass);
-    expect(intervals).toHaveLength(0);
-    view.render(state);
-    intervals = document.getElementsByClassName(intervalCSSClass);
-    expect(intervals).toHaveLength(1);
-    expect(window.getComputedStyle(intervals[0]).display).toBe('block');
-  });
+test('Interval', () => {
+  document.body.innerHTML = '<div id="root"></div>';
+  const el = document.querySelector('#root');
+  const view = new View(el as HTMLElement);
 
-  test('should position interval relative to beginning of track (horizontal)', () => {
-    document.body.innerHTML = '<div id="root"></div>';
-    const el = document.querySelector('#root');
-    const view = new View(el as HTMLElement);
-    view.render(state);
-    const intervals = document.getElementsByClassName(intervalCSSClass);
-    expect(window.getComputedStyle(intervals[0]).left).toBe('20%');
-    expect(window.getComputedStyle(intervals[0]).width).toBe('20%');
-  });
+  fc.assert(
+    fc.property(makeState(), state => {
+      // should position interval relative to beginning of track
+      view.render(state);
+      const intervalElements = document.getElementsByClassName(
+        intervalCSSClass,
+      );
+      state.intervals
+        .filter(prop('isVisible'))
+        .forEach(({ from, to, orientation }, idx) => {
+          const origin = convertOrientationToOrigin(orientation);
+          const dimension = orientation === 'horizontal' ? 'width' : 'height';
 
-  test('should position interval relative to beginning of track (vertical)', () => {
-    // eslint-disable-next-line no-shadow
-    const state: State = {
-      cssClass,
-      track: {
-        orientation: 'vertical',
-        cssClass: trackCSSClass,
-        role: 'track',
-      },
-      intervals: [
-        {
-          id: 'interval_0',
-          from: 0,
-          to: 20,
-          cssClass: intervalCSSClass,
-          orientation: 'vertical',
-          isVisible: false,
-          role: 'interval',
-        },
-        {
-          id: 'interval_1',
-          from: 20,
-          to: 40,
-          cssClass: intervalCSSClass,
-          orientation: 'vertical',
-          isVisible: true,
-          role: 'interval',
-        },
-        {
-          id: 'interval_2',
-          from: 40,
-          to: 100,
-          cssClass: intervalCSSClass,
-          orientation: 'vertical',
-          isVisible: false,
-          role: 'interval',
-        },
-      ],
-      handles: [
-        {
-          id: 'handle_0',
-          position: 20,
-          cssClass: handleCSSClass,
-          orientation: 'vertical',
-          isActive: false,
-          role: 'handle',
-        },
-        {
-          id: 'handle_1',
-          position: 40,
-          cssClass: handleCSSClass,
-          orientation: 'vertical',
-          isActive: false,
-          role: 'handle',
-        },
-      ],
-      tooltips: [
-        {
-          id: 'tooltip_0',
-          position: 20,
-          content: '20',
-          orientation: 'vertical',
-          cssClass: tooltipCSSClass,
-          isVisible: true,
-          hasCollisions: false,
-          role: 'tooltip',
-        },
-        {
-          id: 'tooltip_1',
-          position: 40,
-          content: '40',
-          orientation: 'vertical',
-          cssClass: tooltipCSSClass,
-          isVisible: true,
-          hasCollisions: false,
-          role: 'tooltip',
-        },
-      ],
-      grid: {
-        cssClass: 'range-slider__grid',
-        orientation: 'vertical',
-        isVisible: false,
-        cells: [],
-        min: 0,
-        max: 100,
-        role: 'grid',
-      },
-    };
+          const originValue = window.getComputedStyle(intervalElements[idx])[
+            origin
+          ];
+          const dimensionValue = window.getComputedStyle(intervalElements[idx])[
+            dimension
+          ];
 
-    document.body.innerHTML = '<div id="root"></div>';
-    const el = document.querySelector('#root');
-    const view = new View(el as HTMLElement);
-    view.render(state);
-    const intervals = document.getElementsByClassName(intervalCSSClass);
-    expect(window.getComputedStyle(intervals[0]).bottom).toBe('20%');
-    expect(window.getComputedStyle(intervals[0]).height).toBe('20%');
-  });
+          expect(originValue).toBe(`${from}%`);
+          expect(dimensionValue).toBe(`${to - from}%`);
+        });
+    }),
+    { numRuns: 10 },
+  );
 });
